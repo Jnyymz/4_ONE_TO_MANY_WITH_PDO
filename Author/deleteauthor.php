00edbc824cb5dcae411811f291eff5af161f256c
Jnyymz <152615980+Jnyymz@users.noreply.github.com>
2024-10-22 17:35:36 +0800

    Add files via upload

---
+++ Author/deleteauthor.php
@@ -0,0 +1,30 @@
+<?php require_once 'core/models.php'; ?>
+<?php require_once 'core/dbConfig.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<h1>Are you sure you want to delete this Author?</h1>
+	<?php $getAuthorByID = getAuthorByID($pdo, $_GET['authorID']); ?>
+	<div class="container" style="border-style: solid; height: 400px;">
+	    <h2>Author ID: <?php echo $getAuthorByID['authorID']; ?></h2>
+		<h2>First Name: <?php echo $getAuthorByID['firstname']; ?></h2>
+		<h2>Last Name: <?php echo $getAuthorByID['lastname']; ?></h2>
+		<h2>Nationality: <?php echo $getAuthorByID['nationality']; ?></h2>
+		<h2>Contact Info: <?php echo $getAuthorByID['contactInfo']; ?></h2>
+		<h2>Date Added: <?php echo $getAuthorByID['dateAdded']; ?></h2>
+
+		<div class="deleteBtn" style="float: right; margin-right: 10px;">
+			<form action="core/handleForms.php?authorID=<?php echo $_GET['authorID']; ?>" method="POST">
+				<input type="submit" name="deleteAuthorBtn" value="Delete">
+			</form>			
+		</div>	
+
+	</div>
+</body>
+</html>
---
+++ Author/index.php
@@ -0,0 +1,57 @@
+<?php require_once 'core/dbConfig.php'; ?>
+<?php require_once 'core/models.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<h1>Welcome To Book Store Management System. Add new Authors!</h1>
+	<form action="core/handleForms.php" method="POST">
+		<p>
+			<label for="firstname">First Name: </label> 
+			<input type="text" name="firstname">
+		</p>
+		<p>
+			<label for="lastname">Last Name: </label> 
+			<input type="text" name="lastname">
+		</p>
+		<p>
+			<label for="nationality">Nationality: </label> 
+			<input type="text" name="nationality">
+		</p>
+		<p>
+			<label for="contactInfo">Contact Info: </label> 
+			<input type="text" name="contactInfo">
+		</p>
+		<p>
+			<label for="dateAdded">Date: </label> 
+			<input type="date" name="dateAdded">
+			<input type="submit" name="insertAuthorBtn">
+		</p>
+	</form>
+	<?php $getAllAuthors = getAllAuthors($pdo); ?>
+	<?php foreach ($getAllAuthors as $row) { ?>
+	<div class="container" style="border-style: solid; width: 50%; height: 300px; margin-top: 20px;">
+	    <h3>Author ID: <?php echo $row['authorID']; ?></h3>
+	    <h3>First Name: <?php echo $row['firstname']; ?></h3>
+		<h3>Last Name: <?php echo $row['lastname']; ?></h3>
+		<h3>Nationality: <?php echo $row['nationality']; ?></h3>
+		<h3>Contact Info: <?php echo $row['contactInfo']; ?></h3>
+		<h3>Date Added: <?php echo $row['dateAdded']; ?></h3>
+
+
+		<div class="editAndDelete" style="float: right; margin-right: 20px;">
+			<a href="viewbooks.php?authorID=<?php echo $row['authorID']; ?>">View Books</a>
+			<a href="editauthor.php?authorID=<?php echo $row['authorID']; ?>">Edit</a>
+			<a href="deleteauthor.php?authorID=<?php echo $row['authorID']; ?>">Delete</a>
+		</div>
+
+
+	</div> 
+	<?php } ?>
+</body>
+</html>
